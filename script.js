b07a93ef709266eb8966cef460161403d1fdc1e4
KhanBazeela <bazeelakhan9@gmail.com>
2024-08-23 20:34:01 +0530

    XX

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ document.getElementById('showButton').add EventListener('click', function() {
     daysList.innerHTML = '';
 
     // Create a list item for each day and add it to the unordered list
+     // Loop through the array of days and create list items for each day
     daysOfWeek.forEach(day => {
-        const listItem = document.createElement('li');
-        listItem.textContent = day;
-        daysList.appendChild(listItem);
+        const listItem = document.createElement('li');// Create a new list item element
+        listItem.textContent = day;// Set the text content of the list item to the current day
+        daysList.appendChild(listItem); // Append the list item to the unordered list
     });
 });
